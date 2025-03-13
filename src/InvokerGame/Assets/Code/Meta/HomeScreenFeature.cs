333de11f479867ebf71a900f515d4e2669a377a6
Nikita <toropovnik1802@gmail.com>
2025-03-13 17:43:01 +0300

    update

---
+++ src/InvokerGame/Assets/Code/Meta/HomeScreenFeature.cs
@@ -0,0 +1,13 @@
+﻿using Code.Common.Destruct;
+using Code.Infrastructure.Systems;
+
+namespace Code.Meta
+{
+    public class HomeScreenFeature : Feature
+    {
+        public HomeScreenFeature(ISystemFactory systems)
+        {
+            Add(systems.Create<ProcessDestructedFeature>());
+        }        
+    }
+}
